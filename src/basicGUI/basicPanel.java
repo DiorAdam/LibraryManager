531677a164885b6de39b941bf3efc3cf9c09dfd2
fadel <fadelsougou@gmail.com>
2020-10-12 14:58:00 +0200

    basic frame extending JFrame that will be used for all our application windows

---
+++ src/basicGUI/basicPanel.java
@@ -0,0 +1,9 @@
+package basicGUI;
+import javax.swing.JPanel;
+import java.awt.Color;
+
+public class basicPanel extends JPanel{
+    public basicPanel(){
+        this.setBackground(Color.gray);
+    }
+}
