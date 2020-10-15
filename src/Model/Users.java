c42e5b8d39eca4e2c29806118cd7b624b3129776
fadel <fadelsougou@gmail.com>
2020-10-16 00:48:23 +0200

    add / delete / select in UsersTab using jdbc

--- src/Model/Users.java
+++ src/Model/Users.java
@@ -7,39 +7,92 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
+import java.sql.ResultSet;
+
 
 
 public class Users {
     String url;
-    Connection conn;
-    private Users() {
+    public Users(){
         url = "jdbc:sqlite:"+"D:\\k1gilo\\ST5_capteurs\\TP_DB\\TP_DB.db";
+    }
+    private Connection connect() {
+        Connection conn = null;
         try {
             //Class.forName("org.sqlite.JDBC");
             conn = DriverManager.getConnection(url);
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
+        return conn;
     }
 
     public void add(Integer UserId, String name, String firstName, String email) {
         String sql = "INSERT INTO UsersTab(userId, name, firstName, email) VALUES(?,?,?,?)";
 
-        try (PreparedStatement pstmt = conn.prepareStatement(sql)){
+        try (Connection conn = connect(); PreparedStatement stmt = conn.prepareStatement(sql)){
 
-            pstmt.setInt(1, UserId);
-            pstmt.setString(2, name);
-            pstmt.setString(3, firstName);
-            pstmt.setString(4, email);
-            pstmt.executeUpdate();
+            stmt.setInt(1, UserId);
+            stmt.setString(2, name);
+            stmt.setString(3, firstName);
+            stmt.setString(4, email);
+            stmt.executeUpdate();
         }
         catch (SQLException e) {
             System.out.println(e.getMessage());
         }
     }
 
+    public void del(String email){
+        String sql = "Delete from UsersTab where email = ?";
+
+        try ( Connection conn = connect(); PreparedStatement stmt = conn.prepareStatement(sql)){
+            stmt.setString(1, email);
+            stmt.executeUpdate();
+        }
+        catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void del (int i){
+        String sql = "Delete from UsersTab where UserID = ?";
+
+        try ( Connection conn = connect(); PreparedStatement stmt = conn.prepareStatement(sql)){
+            stmt.setInt(1, i);
+            stmt.executeUpdate();
+        }
+        catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public String[] select (String email){
+        String sql = "Select name, firstName from UsersTab where email = ?";
+        String[] ans = new String[2];
+
+        try (Connection conn = this.connect();
+             PreparedStatement stmt = conn.prepareStatement(sql))
+        {
+            stmt.setString(1, email);
+            ResultSet rs = stmt.executeQuery();
+
+            ans[0] = rs.getString("name");
+            ans[1] = rs.getString("firstName");
+        }
+        catch(Exception e){
+            System.err.println(e.getMessage());
+        }
+        return ans;
+    }
+
     public static void main(String[] args){
         Users a = new Users();
-        a.add(1, "xy", "zzz", "xy.zzz@email.com");
+        //a.add(2, "ab", "ccc", "ab.ccc@email.com");
+        String[] name = a.select("ab.ccc@email.com");
+        System.out.println(name[0] + "  " + name[1]);
+        a.del(2);
+        name = a.select("ab.ccc@email.com");
+        System.out.println(name[0] + "  " + name[1]);
     }
 }
